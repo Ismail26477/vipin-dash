25ca6c195d7abe1516cb69116e82112f559386b7
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-04-22 12:55:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => ({
         secure: false,
       },
     },
+    middlewareMode: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
